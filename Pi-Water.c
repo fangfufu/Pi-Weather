4e6fd465a7c15fa5972d86567886ca39788bfcbf
Fufu Fang <fangfufu@fangfufu.co.uk>
2013-09-09 00:28:21 +0800

    improved error code

--- Pi-Water.c
+++ Pi-Water.c
@@ -9,28 +9,29 @@
 int main(int argc, const char *argv[])
 {
     if (argc == 1) {
-        fprintf(stderr, 
+        fprintf(stderr,
                 "Usage: Pi-Water TIME\nThe unit for TIME is in seconds.\n" );
-        exit(1);
+        exit(-1);
     }
     int sec = atoi(argv[1]);
     if (sec == 0) {
-        fprintf(stderr, 
+        fprintf(stderr,
                 "Please supply a valid number.\n" );
-        exit(2);
+        exit(-2);
     }
     printf("Turing on the pump for %d seconds...", sec);
     exp_open();
-            darlington_on(1);
-            darlington_on(2);
-            darlington_on(3);
-            darlington_on(4);
+    int r = 0;
+    r += darlington_on(1);
+    r += darlington_on(2);
+    r += darlington_on(3);
+    r += darlington_on(4);
     sleep(sec);
-            darlington_off(1);
-            darlington_off(2);
-            darlington_off(3);
-            darlington_off(4);
+    r += darlington_off(1);
+    r += darlington_off(2);
+    r += darlington_off(3);
+    r += darlington_off(4);
     exp_close();
     printf("Done!\n");
-    return 0;
+    return r;
 }
